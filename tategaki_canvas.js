1e8fa235487771b5fb6ad6fd6ee6300287d2bd6c
mogamoga <mogamoga.g1thu6@gmail.com>
2024-05-13 06:46:44 +0900

    '

--- tategaki_canvas.js
+++ tategaki_canvas.js
@@ -1,19 +1,19 @@
 
 function cretaeTategakiCanvas(text, font, shouldHankakuVertical = false) {
-    const canvas = document.createElement("canvas");
+    const canvas = document.createElement('canvas');
 
-    canvas.style.position = "fixed";
-    canvas.style.top = "0";
-    canvas.style.left = "110%";
-    canvas.style.writingMode = "vertical-rl";
+    canvas.style.position = 'fixed';
+    canvas.style.top = '0';
+    canvas.style.left = '110%';
+    canvas.style.writingMode = 'vertical-rl';
     if (shouldHankakuVertical) {
-        canvas.style.textOrientation = "upright";
+        canvas.style.textOrientation = 'upright';
     }
     document.body.append(canvas);
 
-    const context = canvas.getContext("2d");
+    const context = canvas.getContext('2d');
     context.font = font;
-    context.textBaseline = "top";
+    context.textBaseline = 'top';
     const measure = context.measureText(text);
     const textWidth = measure.width;
     const textHeight = Math.abs(measure.actualBoundingBoxAscent) + measure.actualBoundingBoxDescent;
@@ -23,14 +23,18 @@ function cretaeTategakiCanvas(text, font, shouldHankakuVertical = false) {
     context.rotate(Math.PI / 2);
 
     context.font = font;
-    context.textBaseline = "top";
+    context.textBaseline = 'top';
     context.fillText(text, 0, -textHeight);
 
     document.body.removeChild(canvas);
-    canvas.style.position = "";
-    canvas.style.top = "";
-    canvas.style.left = "";
-    canvas.style.writingMode = "";
+    canvas.style.position = '';
+    canvas.style.top = '';
+    canvas.style.left = '';
+    canvas.style.writingMode = '';
 
     return canvas;
+}
+
+if (typeof module !== 'undefined' && module.exports) {
+    module.exports = cretaeTategakiCanvas;
 }
