16a9a1573b539721c21189975b833c5a56613638
mogamoga <mogamoga.g1thu6@gmail.com>
2024-05-16 04:10:22 +0900

    measure

--- vertical_text_canvas.js
+++ vertical_text_canvas.js
@@ -2,23 +2,30 @@
 (() => {
     const createVerticalTextCanvas = (text, font, options = {}) => {
         return _createVerticalTextCanvas(text, font, false, false, 0, options);
-    }
+    };
     
     const createAllVerticalTextCanvas = (text, font, options = {}) => {
         return _createVerticalTextCanvas(text, font, true, false, 0, options);
-    }
+    };
 
     const createVerticalTextStrokeCanvas = (text, font, options = {}, maxWidth) => {
         return _createVerticalTextCanvas(text, font, false, true, maxWidth, options);
-    }
+    };
     
     const createAllVerticalTextStrokeCanvas = (text, font, options = {}, maxWidth) => {
         return _createVerticalTextCanvas(text, font, true, true, maxWidth, options);
-    }
-    
-    const _createVerticalTextCanvas = (text, font, shouldHankakuVertical, useStrokeText, maxWidth, options) => {
+    };
+
+    const measureVerticalTextCanvasSize = (text, font, options = {}) => {
+        return _measureVerticalTextCanvasSize(text, font, false, options);
+    };
+
+    const measureAllVerticalTextCanvasSize = (text, font, options = {}) => {
+        return _measureVerticalTextCanvasSize(text, font, true, options);
+    };
+
+    const appendCanvas = (shouldHankakuVertical) => {
         const canvas = document.createElement('canvas');
-    
         canvas.style.position = 'fixed';
         canvas.style.top = '0';
         canvas.style.left = '110%';
@@ -27,7 +34,36 @@
             canvas.style.textOrientation = 'upright';
         }
         document.body.append(canvas);
+        return canvas;
+    };
+
+    const removeCanvas = (canvas) => {
+        document.body.removeChild(canvas);
+        canvas.style.position = '';
+        canvas.style.top = '';
+        canvas.style.left = '';
+        canvas.style.writingMode = '';
+    };
+
+    const _measureVerticalTextCanvasSize = (text, font, shouldHankakuVertical, options = {}) => {
+        const canvas = appendCanvas(shouldHankakuVertical);
+        const context = canvas.getContext('2d');
+        context.font = font;
+        context.textBaseline = 'top';
+        Object.assign(context, options);
+        const measure = context.measureText(text);
+
+        // 90度回転させるため縦横が入れ替わる
+        const height = measure.width;
+        const width = Math.abs(measure.actualBoundingBoxAscent) + measure.actualBoundingBoxDescent;
+        
+        removeCanvas(canvas);
+
+        return { width, height };
+    };
     
+    const _createVerticalTextCanvas = (text, font, shouldHankakuVertical, useStrokeText, maxWidth, options) => {
+        const canvas = appendCanvas(shouldHankakuVertical);
         const context = canvas.getContext('2d');
         context.font = font;
         context.textBaseline = 'top';
@@ -55,19 +91,16 @@
             context.fillText(text, 0, -textHeight);
         }
     
-        document.body.removeChild(canvas);
-        canvas.style.position = '';
-        canvas.style.top = '';
-        canvas.style.left = '';
-        canvas.style.writingMode = '';
-    
+        removeCanvas(canvas);
+        
         return canvas;
-    }
+    };
 
     if (typeof module !== 'undefined' && module.exports) {
         module.exports = {
             createVerticalTextCanvas, createAllVerticalTextCanvas,
-            createVerticalTextStrokeCanvas, createAllVerticalTextStrokeCanvas
+            createVerticalTextStrokeCanvas, createAllVerticalTextStrokeCanvas,
+            measureVerticalTextCanvasSize, measureAllVerticalTextCanvasSize
         };
     }
     else if (typeof window !== 'undefined') {
@@ -75,5 +108,7 @@
         window.createAllVerticalTextCanvas = createAllVerticalTextCanvas;
         window.createVerticalTextStrokeCanvas = createVerticalTextStrokeCanvas;
         window.createAllVerticalTextStrokeCanvas = createAllVerticalTextStrokeCanvas;
+        window.measureVerticalTextCanvasSize = measureVerticalTextCanvasSize;
+        window.measureAllVerticalTextCanvasSize = measureAllVerticalTextCanvasSize;
     }
 })();
